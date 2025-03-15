19489fb66eb319a8ba8e0f11a562cd7f322d8d5f
Jay Malaviya <jay.malaviya@kickdrumtech.com>
2025-03-16 00:57:02 +0530

    feature/cors: appended api/v1 at outputs

--- iac/modules/api_gateway/outputs.tf
+++ iac/modules/api_gateway/outputs.tf
@@ -1,18 +1,3 @@
-output "api_id" {
-  description = "The ID of the REST API Gateway"
-  value       = aws_api_gateway_rest_api.api.id
-}
-
-output "api_arn" {
-  description = "The ARN of the REST API Gateway"
-  value       = aws_api_gateway_rest_api.api.arn
-}
-
-output "execution_arn" {
-  description = "The execution ARN of the REST API Gateway"
-  value       = aws_api_gateway_rest_api.api.execution_arn
-}
-
 output "rest_api_id" {
   description = "The ID of the REST API"
   value       = aws_api_gateway_rest_api.api.id
