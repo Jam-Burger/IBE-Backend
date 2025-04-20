774e2402fecbdbf80b00768a38ee79b3dda3a5dd
Jay Malaviya <jay.malaviya@kickdrumtech.com>
2025-04-20 21:36:34 +0530

    feature/x-ray: updated application name

--- iac/modules/lambda/housekeeping_service/index.mjs
+++ iac/modules/lambda/housekeeping_service/index.mjs
@@ -27,13 +27,13 @@ import {sendEmails} from "./services/emailService.mjs";
 const {Pool} = pkg;
 
 // Enable X-Ray tracing for PostgreSQL
-const pgPool = AWSXRay.capturePostgres(new Pool({
+const pgPool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-}));
+});
 
 export const handler = async (event) => {
     const today = toIsoString(new Date());
