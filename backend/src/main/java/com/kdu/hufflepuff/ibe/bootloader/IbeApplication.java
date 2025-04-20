2865e2d1c11b8039db9f04ae73476bd8f70eea58
Mirza Atif beg <mirzaatif.beg@kickdrumtech.com>
2025-04-20 22:16:45 +0530

    feature/caching:Adding Redis Cache Configuration and Caching few Api Result

--- backend/src/main/java/com/kdu/hufflepuff/ibe/bootloader/IbeApplication.java
+++ backend/src/main/java/com/kdu/hufflepuff/ibe/bootloader/IbeApplication.java
@@ -3,12 +3,14 @@ package com.kdu.hufflepuff.ibe.bootloader;
 import com.kdu.hufflepuff.ibe.config.ProjectConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @Import({ProjectConfig.class})
 public class IbeApplication {
     public static void main(String[] args) {
