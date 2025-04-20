04e7244a618f8855a825a4fda19f197b90a872be
Jay Malaviya <jay.malaviya@kickdrumtech.com>
2025-04-20 22:56:32 +0530

    fix/workflow: updated sh file for yarn install

--- iac/modules/lambda/build.sh
+++ iac/modules/lambda/build.sh
@@ -5,18 +5,18 @@ set -e
 
 echo "Building Lambda functions..."
 
-# Build housekeeping_service
-echo "Building housekeeping_service..."
-cd "$(dirname "$0")/housekeeping_service"
-yarn install --frozen-lockfile
-zip -r "../housekeeping_service.zip" . -x "node_modules/.cache/*" "*.git*"
-
-cd ..
-
 # Build promotional_email_sender
 echo "Building promotional_email_sender..."
 cd "$(dirname "$0")/promotional_email_sender"
 yarn install --frozen-lockfile
 zip -r "../promotional_email_sender.zip" . -x "node_modules/.cache/*" "*.git*"
+
+cd ..
+
+# Build housekeeping_service
+echo "Building housekeeping_service..."
+cd "$(dirname "$0")/housekeeping_service"
+yarn install --frozen-lockfile
+zip -r "../housekeeping_service.zip" . -x "node_modules/.cache/*" "*.git*"
 
 echo "All builds completed successfully!" 
